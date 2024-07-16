ea096d9db4e0cb40adff49afc04100d7a5741221
Thiyunu <dinal.bandara@gmail.com>
2024-07-16 22:18:00 +0530

    update: Added a Form to input relevant data

---
+++ app/(root)/addNewProduct/page.tsx
@@ -0,0 +1,10 @@
+import AddnewComponent from "@/components/add-new-item/add-new";
+const AddNewPage = () => {
+    return ( 
+        <div>
+            <AddnewComponent/>
+        </div>
+     );
+}
+ 
+export default AddNewPage;
---
+++ components/add-new-item/add-new.tsx
@@ -0,0 +1,10 @@
+import Form from "./form";
+const AddnewComponent = () => {
+    return ( 
+        <div className="flex w-screen m-6 justify-center ">
+           <Form/>
+        </div>
+     );
+}
+ 
+export default AddnewComponent;
