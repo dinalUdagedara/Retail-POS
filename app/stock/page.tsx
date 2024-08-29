a47cee7da6145dcd91ee0fd8a204b3030be59b37
Thiyunu <dinal.bandara@gmail.com>
2024-08-29 11:37:01 +0530

    feat: Added Barcode Scanner

---
+++ app/stock/page.tsx
@@ -0,0 +1,10 @@
+import { StockTable } from "@/components/stock-dashboard/stock-table";
+const StockPage = () => {
+  return (
+    <div className="w-full p-20">
+      <StockTable />
+    </div>
+  );
+};
+
+export default StockPage;
